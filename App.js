734015e700ef3753ce11f41d0f1dad812e351ad6
MiamiPublication <145936310+MiamiPublication@users.noreply.github.com>
2023-09-26 21:58:26 +0200

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! tittar det funkar</Text>
-      <Text>Mera text nu med en förändring yttaligare text</Text>
+      <Text>Mera text nu med en förändring yttaligare text... oj vad mycket text</Text>
       <StatusBar style="auto" />
     </View>
   );
